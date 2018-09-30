75fc55c7501fea94e67fd66784d958b2d31c3de9
Kameswari Devi Ayyagari <devi@cs.utah.edu>
2018-09-29 21:29:06 -0700

    Create a wrapper in lib of dicom to hdf5 convertor for filr name and volume

--- src/dicomUtils/h5Convertor.py
+++ src/dicomUtils/h5Convertor.py
@@ -30,6 +30,22 @@ def dicomToH5(files, outFile):
     return 0
 
 def h5ToDicom(inputDicomFiles, hdf5File, outputDicomDir):
+    array3D = readVolumeFromH5File(hdf5File)
+    if(array3D is None):
+        return False
+    volume3DToDicom(inputDicomFiles, array3D, outputDicomDir)
+    return True
+
+def readVolumeFromH5File(hdf5File):
+    h5File = h5py.File(hdf5File,'r')
+
+    if H5_DATASET_NAME not in h5File.keys():
+        return None
+
+    dset = h5File[H5_DATASET_NAME]
+    return np.array(dset)
+
+def volume3DToDicom(inputDicomFiles, array3D, outputDicomDir):
     sliceLocations = []
     for file in inputDicomFiles:
         dataset = pydicom.dcmread(file)
@@ -40,14 +56,6 @@ def h5ToDicom(inputDicomFiles, hdf5File, outputDicomDir):
         sliceLocationIndeces[sliceLoc] = i
     del sliceLocations[:]
 
-    h5File = h5py.File(hdf5File,'r')
-
-    if H5_DATASET_NAME not in h5File.keys():
-        return -1
-
-    dset = h5File[H5_DATASET_NAME]
-    array3D = np.array(dset)
-
     for file in inputDicomFiles:
         dicomFile = pydicom.dcmread(file)
         npArray = array3D[:,:,sliceLocationIndeces[dicomFile.SliceLocation]]
@@ -55,5 +63,3 @@ def h5ToDicom(inputDicomFiles, hdf5File, outputDicomDir):
         npArray = npArray.astype(dicomFile.pixel_array.dtype)
         dicomFile.PixelData = npArray.tobytes()
         dicomFile.save_as(outputDicomDir+"/"+file.split("/")[-1])
-
-    return 0
